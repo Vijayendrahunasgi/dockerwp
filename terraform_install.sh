51c14b11f5535cb8b34c7acbb0ad09986c163f54
Vijayendrahunasgi <134865648+Vijayendrahunasgi@users.noreply.github.com>
2023-06-18 15:47:38 +0530

    Update terraform_install.sh

--- terraform_install.sh
+++ terraform_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
